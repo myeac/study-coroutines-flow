cdda15add1b6450c814026f46c8ea989a89ed720
MiguelY <miguelyeac@hotmail.com>
2025-10-05 19:06:14 -0300

    usando retry/repeat

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,38 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
-
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            val numberOfTries = 2
+            try {
+                repeat(numberOfTries) {
+                    try {
+                        loadRecentAndroidVersions()
+                        return@launch
+                    } catch (e: Exception) {
+                        Timber.e(e)
+                    }
+                }
+                loadRecentAndroidVersions()
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("error en la solicitacion!")
+            }
+        }
     }
 
+    private suspend fun loadRecentAndroidVersions() {
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions)
+    }
 }
