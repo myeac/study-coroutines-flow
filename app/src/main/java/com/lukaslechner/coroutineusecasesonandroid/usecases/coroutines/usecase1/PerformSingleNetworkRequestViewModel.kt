0382bec5b1b5134e6fa66e7170b7dc5898a42538
MiguelY <miguelyeac@hotmail.com>
2025-10-05 14:55:34 -0300

    coroutine calls con errores y exito - use case 1

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase1/PerformSingleNetworkRequestViewModel.kt
@@ -1,18 +1,35 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class PerformSingleNetworkRequestViewModel(
-    private val mockApi: MockApi = mockApi()
+    private val mockApi: MockApi = mockApi(),
+    private val mockApiError: MockApi = mockApiError()
 ) : BaseViewModel<UiState>() {
 
     fun performSingleNetworkRequest() {
-        //alternar entre cualquiera de estos estados
+        uiState.value = UiState.Loading
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = mockApiError.getRecentAndroidVersions()
+                uiState.value = UiState.Success(recentAndroidVersions)
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("fallo la comunicacion con la red")
+            }
+        }
+    }
+
+    private fun ejemplosUIState() {
         uiState.value = UiState.Loading
 
         uiState.value = UiState.Error("Algo malo paso!!! :c")
 
-        uiState.value= UiState.Success(listOf())
+        uiState.value = UiState.Success(listOf())
     }
+
 }
