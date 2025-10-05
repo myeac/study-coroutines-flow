1324530b142902c1cf09721e9350e34837427003
MiguelY <miguelyeac@hotmail.com>
2025-10-05 20:09:51 -0300

    usando timeout y retry, ejercicio

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase7/TimeoutAndRetryViewModel.kt
@@ -1,7 +1,14 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase7
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
+import timber.log.Timber
 
 class TimeoutAndRetryViewModel(
     private val api: MockApi = mockApi()
@@ -12,10 +19,55 @@ class TimeoutAndRetryViewModel(
         val numberOfRetries = 2
         val timeout = 1000L
 
-        // TODO: Exercise 3
-        // switch to branch "coroutine_course_full" to see solution
+        val oreoVersionDeferred = viewModelScope.async {
+            retryWithTimeOut(numberOfRetries, timeout) {
+                api.getAndroidVersionFeatures(27)
+            }
+        }
 
-        // run api.getAndroidVersionFeatures(27) and api.getAndroidVersionFeatures(28) in parallel
+        val pieVersionDeferred = viewModelScope.async {
+            retryWithTimeOut(numberOfRetries, timeout) {
+                api.getAndroidVersionFeatures(27)
+            }
+        }
 
+        viewModelScope.launch {
+            try {
+                val versionFeatures = listOf(
+                    oreoVersionDeferred,
+                    pieVersionDeferred,
+                ).awaitAll()
+                uiState.value = UiState.Success(versionFeatures)
+            } catch (e: Exception) {
+                Timber.e(e)
+                uiState.value = UiState.Error("error en la comunicacion servidor!!")
+            }
+        }
     }
+}
+
+suspend fun <T> retryWithTimeOut(
+    numberOfTries: Int,
+    timeout: Long,
+    block: suspend () -> T
+) = retry(numberOfTries) {
+    withTimeout(timeout) {
+        block()
+    }
+}
+
+suspend fun <T> retry(
+    numberOfTries: Int,
+    delayBetweenRetries: Long = 100,
+    block: suspend () -> T
+): T {
+    repeat(numberOfTries) {
+        try {
+            return block()
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+        delay(delayBetweenRetries)
+    }
+    return block()
 }
