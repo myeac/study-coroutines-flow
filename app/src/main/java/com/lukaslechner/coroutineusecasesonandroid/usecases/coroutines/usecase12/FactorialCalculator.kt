124944e77a7d2ee2f41c06a914ff70a703cb0f1c
MiguelY <miguelyeac@hotmail.com>
2025-10-25 22:45:33 -0300

    usar varios coroutines para calculos

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/FactorialCalculator.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase12/FactorialCalculator.kt
@@ -2,19 +2,32 @@ package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase1
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.withContext
 import java.math.BigInteger
 
 class FactorialCalculator(
     private val defaultDispatcher: CoroutineDispatcher = Dispatchers.Default
 ) {
 
-    fun calculateFactorial(
+    suspend fun calculateFactorial(
         factorialOf: Int,
         numberOfCoroutines: Int
     ): BigInteger {
 
         // TODO: create sub range list *on background thread*
-        val subRanges = createSubRangeList(factorialOf, numberOfCoroutines)
+        withContext(Dispatchers.Default){
+            val subRanges = createSubRangeList(factorialOf, numberOfCoroutines)
+            subRanges.map { subRanges->
+                async {
+                    calculateFactorialOfSubRange(subRanges)
+                }
+            }.awaitAll()
+                .fold(BigInteger.ONE, {acc, element ->
+                    acc.multiply(element)
+                })
+        }
 
 
         // TODO: calculate factorial of each subrange in separate coroutine
